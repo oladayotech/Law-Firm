6ebd7f65211cbcbbb21ad9cb7dc921f6bba30e07
oladayotech <d1arogundade@gmail.com>
2025-07-03 14:05:16 +0100

    Ready for deployment

--- Law_firm/wsgi.py
+++ Law_firm/wsgi.py
@@ -13,4 +13,23 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Law_firm.settings')
 
+import os
+import django
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Law_firm.settings')
+
+# 👇👇 AUTO RUN MIGRATIONS
+django.setup()
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
+
 application = get_wsgi_application()
+
+
+application = get_wsgi_application()
+
+
