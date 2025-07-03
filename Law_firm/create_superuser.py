a1c5c88e7c503999688f612944dd927ca975eb21
oladayotech <d1arogundade@gmail.com>
2025-07-03 19:37:28 +0100

    Ready for deployment

--- Law_firm/create_superuser.py
+++ Law_firm/create_superuser.py
@@ -8,8 +8,8 @@ django.setup()
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@gmail.com", "Adminpassword1234")
+if not User.objects.filter(username="dayo").exists():
+    User.objects.create_superuser("dayo", "dayo@gmail.com", "1234")
     print("✅ Superuser created!")
 else:
     print("⚠️ Superuser already exists.")
