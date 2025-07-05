a3482b7b5b3ba92d1aba88e18a31fd21b921f386
oladayotech <d1arogundade@gmail.com>
2025-07-05 01:27:51 +0100

    Ready for deployment

--- Law_firm/urls.py
+++ Law_firm/urls.py
@@ -18,9 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+
+
+from O_H_SHOKUBI.sitemaps import NewsSitemap
+
+sitemaps = {
+    'news': NewsSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('O_H_SHOKUBI.urls'))
+    path('', include('O_H_SHOKUBI.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ O_H_SHOKUBI/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from .models import News  # replace with your main model
+
+class NewsSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return News.objects.all()
+
+    def lastmod(self, obj):
+        return obj.updated  # or obj.created if no updated
